8d60fc0d628b9fc206be2278e77d4ab32ae4f0c7
zhongqifeng <1121021693@qq.com>
2024-05-18 15:54:24 +0800

    feat: Adjusting directory structure

--- src/temp/tempFiles/data.js
+++
@@ -1,5 +0,0 @@
-{
-  "newResourceOutPutPath": "D:\\ShanChuan\\新建文件夹\\mosaic_project\\hospital_output",
-  "newProjectPath": "D:\\ShanChuan\\新建文件夹\\mosaic_project\\hospital_pro",
-  "currentMosaicProjectPath": "D:\\ShanChuan\\新建文件夹/mosaic_project"
-}
--- src/temp/tempFiles/serverConfig.js
+++
@@ -1,7 +0,0 @@
-{
-  "192.168.22.1": {
-    "username": "root",
-    "ip": "192.168.22.1",
-    "deployDirectory": "/usr/local/esign/apps/front_test"
-  }
-}
